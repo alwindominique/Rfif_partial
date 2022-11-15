14a8778e11f732632cc5f6e719404c2d03efd343
Alwin Dominique Lagat <alwindominiquelagat@gmail.com>
2022-11-15 13:46:28 +0800

    Partial Commit

---
+++ home.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>DASHBOARD</title>
+</head>
+<style>
+.main{
+    padding: 0px;
+    margin: 0px;
+}
+
+ .box{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-style: none;
+    background-color: maroon;
+    color: black;
+    margin-top: 300px;
+    transition: 1s;
+    cursor: pointer;
+ }
+ .box:hover{
+    border-radius: 30px;
+    color: white;
+ }  
+ .btn:hover{
+    background-color: lightskyblue;
+ }
+
+</style>
+<body>
+
+    <div class="main">
+        <div class="box">
+            <h1>CSU-APARRI ATTENDANCE MONITORING SYSTEM</h1> 
+        </div>
+            
+            <button class="btn" style="display: flex; justify-content: center; text-align: center; padding: 10px; border-radius: 10px; border-style: none; margin-top: 10px;margin-left: 700px; font-size: 30px; transition: 1s; "><a href="index2.php">LOGOUT</a>
+            </button>
+        
+    </div>
+
+   
+</body>
+</html>
